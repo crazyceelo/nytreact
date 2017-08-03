c2ef26cf5afa2756383e6e9dd45764f341448ee9
Carlos Perez <solracias@gmail.com>
2017-08-02 20:54:11 -0700

    aded a basic main file

---
+++ app/components/Main.js
@@ -0,0 +1,15 @@
+var React = require("react");
+// var Link = require("react-router").Link;
+
+
+class Main extends React.Component {
+    render(){
+        return(
+            <div className="container">
+                <h1>test</h1>
+            </div>
+        )
+    }
+}
+
+module.exports = Main;
--- server.js
+++ server.js
@@ -32,6 +32,13 @@ db.once("open", function(){
     console.log("Mongoose connection successful.");
 });
 
+// /api/saved (get) - your components will use this to query MongoDB for all saved articles
+// /api/saved (post) - your components will use this to save an article to the database
+// /api/saved (delete) - your components will use this to delete a saved article in the database
+    
+/* * (get) - will load your single HTML page (with ReactJS) 
+in public/index.html. Make sure you put this after all other GET routes */
+
 // listener
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
